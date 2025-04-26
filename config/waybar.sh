934ae7a1c96cd8daceb37e11d0bb28aa4d94ccbd
JimJ92120 <jimj.development@gmail.com>
2025-04-27 00:27:46 +0700

    refactor: update build and swith to json

--- config/waybar.sh
+++ config/waybar.sh
@@ -5,7 +5,7 @@ killall waybar
 
 if [[ $USER = "jim" ]]
 then
-  waybar -c ~/.config/waybar/config.jsonrc & -s ~/.config/waybar/style.css
+  waybar -c ~/.config/waybar/config.json & -s ~/.config/waybar/style.css
 else
   waybar &
 fi
